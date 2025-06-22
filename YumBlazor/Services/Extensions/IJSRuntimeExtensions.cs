4cb8a377cab001849cb3c9dcd00071873ab2f7e3
MiguelMods <miguelmodd@gmail.com>
2025-06-22 10:39:32 -0400

    agregando js y otras funciones mas componentes

---
+++ YumBlazor/Services/Extensions/IJSRuntimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static async Task ToastrSuccess(this IJSRuntime runtime, string message)
+            => await runtime.InvokeVoidAsync("showToastr", ToastrConstants.Success, message);
+
+        public static async Task ToastrError(this IJSRuntime runtime, string message) 
+            => await runtime.InvokeVoidAsync("showToastr", ToastrConstants.Error, message);
+
+        public static async Task ToastrInfo(this IJSRuntime runtime, string message) 
+            => await runtime.InvokeVoidAsync("showToastr", ToastrConstants.Info, message);
+
+        public static async Task ToastrWarning(this IJSRuntime runtime, string message) 
+            => await runtime.InvokeVoidAsync("showToastr", ToastrConstants.Warning, message);
+
+        public static async Task Toastr(this IJSRuntime runtime, string type, string message) 
+            => await runtime.InvokeVoidAsync("showToastr", type, message);
+    }
+
+    public static class ToastrConstants
+    {
+        public const string Success = "success";
+        public const string Error = "error";
+        public const string Info = "info";
+        public const string Warning = "warning";
+    }
+}
---
+++ YumBlazor/wwwroot/js/app.js
@@ -0,0 +1,28 @@
+﻿window.showToastr = function (type, messge)
+{
+    if (typeof toastr === 'undefined')
+    {
+        alert('Toastr is not defined. Please ensure toastr.js is included in your project.');
+        return;
+    } else if (typeof toastr[type] !== 'function') {
+        alert(`Toastr type "${type}" is not valid. Valid types are: info, success, warning, error.`);
+        return;
+    }
+        
+    switch (type) {
+        case 'info':
+            toastr.info(messge);
+            break;
+        case 'success':
+            toastr.success(messge);
+            break;
+        case 'warning':
+            toastr.warning(messge);
+            break;
+        case 'error':
+            toastr.error(messge);
+            break;
+        default:
+            toastr.info(messge);
+    }
+}
