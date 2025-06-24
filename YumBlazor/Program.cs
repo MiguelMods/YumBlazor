03497bf046f12cdec1cc0bd8ab1808b0e7243a49
MiguelMods <miguelmodd@gmail.com>
2025-06-23 20:12:23 -0400

    agregando categorias pendientes

--- YumBlazor/Program.cs
+++ YumBlazor/Program.cs
@@ -36,7 +36,7 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
-builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, CategoryRepository>();
 
 var app = builder.Build();
 
